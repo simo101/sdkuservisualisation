e320fb5b1e1029998d335559ef318cb6740bb396
Simon Black <simonblack1@hotmail.com>
2016-02-07 20:57:35 +0000

    Working base loading of navigation

--- script.ts
+++ script.ts
@@ -1,41 +1,83 @@
 /// <reference path='./typings/tsd.d.ts' />
-  
-import {MendixSdkClient, Project, OnlineWorkingCopy} from 'mendixplatformsdk';
-import {IModel, domainmodels} from 'mendixmodelsdk';
+
+import { MendixSdkClient, OnlineWorkingCopy, Project, Revision, Branch } from "mendixplatformsdk";
+import { ModelSdkClient, IModel, projects, domainmodels, microflows, pages, navigation, texts } from "mendixmodelsdk";
 import when = require('when');
-import cred = require("./credentials");
-  
-const username = cred.Credentials.username;
-const apikey = cred.Credentials.apikey
+//const readlineSync = require('readline-sync');
+
+/*
+ * CREDENTIALS
+ */
+const JSONWriter = require('json-writer');
+var jw = new JSONWriter;
+const username = "{USER NAME}";
+const apikey = "{API KEY}";
+const projectId = "{PROJECT ID}";
+const projectName = "{PROJECT NAME}";
+const revNo = -1; // -1 for latest
+const branchName = null // null for mainline
+
 const client = new MendixSdkClient(username, apikey);
- 
-client.platform().createNewApp(`NewApp-${Date.now() }`)
-    .then(project => project.createWorkingCopy())
-    .then(workingCopy => loadDomainModel(workingCopy))
-    .then(workingCopy => {
-        const dm = pickDomainModel(workingCopy);
-        const domainModel = dm.load();
-        let entity = domainmodels.Entity.createIn(domainModel);
-        entity.name = `NewEntity_${Date.now() }`;
-        entity.location = { x: 100, y: 100 };
-        return workingCopy;
-    })
-    .then(workingCopy => workingCopy.commit())
+
+/*
+ * PROJECT TO ANALYZE
+ */
+const project = new Project(client, projectId, projectName);
+
+client.platform().createOnlineWorkingCopy(project, new Revision(revNo, new Branch(project, branchName)))
+    .then(workingCopy => loadNavigationDocuments(workingCopy)
+    .then(navigationDoc => processNavigation(navigationDoc))
     .done(
-        revision => console.log(`Successfully committed revision: ${revision.num() }. Done.`),
+        () => {
+            console.log("Done.");
+        },
         error => {
-            console.log('Something went wrong:');
+            console.log("Something went wrong:");
             console.dir(error);
-        });
+        }));
  
-function loadDomainModel(workingCopy: OnlineWorkingCopy): when.Promise<OnlineWorkingCopy> {
-    const dm = pickDomainModel(workingCopy);
-    return when.promise<OnlineWorkingCopy>((resolve, reject) => {
-        dm.load(dm => resolve(workingCopy));
+function loadNavigationDocuments(workingCopy: OnlineWorkingCopy): when.Promise<navigation.NavigationDocument> {
+    const navigation = pickNavigationDocument(workingCopy);
+    return when.promise<navigation.NavigationDocument>((resolve, reject) => {
+        navigation.load(nav => resolve(nav));
     });
 }
  
-function pickDomainModel(workingCopy: OnlineWorkingCopy): domainmodels.IDomainModel {
-    return workingCopy.model().allDomainModels()
-        .filter(dm => dm.qualifiedName === 'MyFirstModule')[0];
+function pickNavigationDocument(workingCopy: OnlineWorkingCopy): navigation.NavigationDocument {
+    return workingCopy.model().allNavigationDocuments().filter[0];
+}
+
+function processNavigation (navDoc: navigation.NavigationDocument): navigation.NavigationDocument {
+    
+    let homepage = navDoc.desktopProfile.homePage;
+    navDoc.desktopProfile.roleBasedHomePages.forEach(roleBasedHomepage => {
+        let userRoleName = roleBasedHomepage.userRole.name;
+        let rolePage = roleBasedHomepage.page;
+        let roleMicroflow = roleBasedHomepage.microflow;
+        
+    }
+    );
+    if (homepage.page != null){
+        var homepagePage = homepage.page;
+    }else if(homepage.microflow != null){    
+    }
+    else{
+        
+    }
+    return navDoc;
+};
+function userRoles(workingCopy: OnlineWorkingCopy){
+    workingCopy.model().allProjectSecurities()[0].userRoles.forEach(role =>{
+        
+        jw.startDocument().startElement(role.name);
+    }
+    )
+}
+
+function traverseMicroflow(microflow: microflows.Microflow){
+    microflow.objectCollection.objects.filter(o => o instanceof microflows.ShowPageAction || o instanceof microflows.ShowHomePageAction ).forEach((o) => {
+        
+    });
+    microflow.objectCollection.objects.filter(o => o instanceof microflows.MicroflowParameter).forEach((o) => { 
+    });
 }
