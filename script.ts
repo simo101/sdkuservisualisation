c3606357e4946ad2fb811cf8e9bb191dc4cc84c2
Simon Black <simonblack1@hotmail.com>
2016-02-26 17:44:30 +0000

    Working on the traversal of pages.

--- script.ts
+++ script.ts
@@ -8,20 +8,20 @@ import when = require('when');
 /*
  * CREDENTIALS
  */
-var  JSONWriter = require('json-writer');
+var JSONWriter = require('json-writer');
 var fs = require('fs');
-var ws = fs.createWriteStream('/tmp/mendix.json');
+var ws = fs.createWriteStream('./tmp/mendix.json');
 ws.on('close', function() {
-    console.log(fs.readFileSync('/tmp/mendix.json', 'UTF-8'));
+    console.log(fs.readFileSync('./tmp/mendix.json', 'UTF-8'));
 });
-var jw = new JSONWriter(false, function(string, encoding) { 
-      ws.write(string, encoding);
-  });
+var jw = new JSONWriter(false, function(string, encoding) {
+    ws.write(string, encoding);
+});
 
-const username = "{{Username}}";
-const apikey = "{{APIKEY}}";
-const projectId = "{{ProjectID}}";
-const projectName = "{{ProjectName}}";
+const username = "simon.black@mendix.com";
+const apikey = "ba47d0a1-9991-45ee-a14d-d0c1b73d5279";
+const projectId = "bce92469-dd44-4414-a7fb-af659a2cee44";
+const projectName = "NaturesPrideDemo";
 const revNo = -1; // -1 for latest
 const branchName = null // null for mainline
 
@@ -35,120 +35,125 @@ const project = new Project(client, projectId, projectName);
 client.platform().createOnlineWorkingCopy(project, new Revision(revNo, new Branch(project, branchName)))
     .then(workingCopy => processUserNavigation(workingCopy))
     .done(
-        () => {
-            ws.end();
-            console.log("Done.");
-        },
-        error => {
-            console.log("Something went wrong:");
-            console.dir(error);
-        });
+    () => {
         
-function pickNavigationDocument(workingCopy: OnlineWorkingCopy): navigation.INavigationDocument {
-    workingCopy.model().allNavigationDocuments().forEach(nav => {
-        return nav;
+        ws.end();
+        console.log("Done.");
+    },
+    error => {
+        console.log("Something went wrong:");
+        console.dir(error);
     });
-    return null;
+
+function pickNavigationDocument(workingCopy: OnlineWorkingCopy): navigation.INavigationDocument {
+    return workingCopy.model().allNavigationDocuments()[0];
 }
 
-function processNavigation (role: security.IUserRole, navDoc: navigation.NavigationDocument){
-    
+function processNavigation(role: security.IUserRole, navDoc: navigation.NavigationDocument): when.Promise<void> {
+
     let homepage = navDoc.desktopProfile.homePage;
     let usersHomepage = navDoc.desktopProfile.roleBasedHomePages.filter(roleBasedHomepage => roleBasedHomepage.userRole.name === role.name)[0];
-    
-    if (usersHomepage.page != null){
-        jw.writeElement(usersHomepage.page.name);
-        traversePage(usersHomepage.page);
-    }else if(usersHomepage.microflow != null){
-        jw.writeElement(usersHomepage.microflow.name);
-        traverseMicroflow(usersHomepage.microflow);    
+    if (usersHomepage != null){    
+        if (usersHomepage.page != null) {
+            jw.writeElement(usersHomepage.page.name);
+            console.log(`${role.name} homepage = ${usersHomepage.page.name}`);
+            return traversePage(usersHomepage.page).then(_=>{jw.endElement()});
+        } else if (usersHomepage.microflow != null) {
+            jw.writeElement(usersHomepage.microflow.name);
+            console.log(`${role.name} homepage = ${usersHomepage.microflow.name}`);
+            return traverseMicroflow(usersHomepage.microflow).then(_=>{jw.endElement()});
+            
+        }
     }
-    jw.endElement();
-    
 };
-function processUserNavigation(workingCopy: OnlineWorkingCopy){
+function processUserNavigation(workingCopy: OnlineWorkingCopy): when.Promise<void> {
     jw.startDocument('1.0', 'UTF-8');
-    workingCopy.model().allProjectSecurities()[0].userRoles.forEach(role =>{
-        jw.startElement(role.name);
-        loadAsPromise(pickNavigationDocument(workingCopy)).then(
-          navigation =>{
-              processNavigation(role,navigation);
-          }  
-        );
-        jw.endElement();
-    });
-    jw.endDocument();
-    
-}
-
-function traversePage(page: pages.IPage){
-    loadAsPromise(page).then(pageLoaded =>{
-        loadAsPromise(pageLoaded.layoutCall).then(layout =>{
-            var layoutCall = <pages.LayoutCall> layout;
-            layoutCall.arguments.forEach(args =>{
-                var widget = args.widget; 
-                if(widget instanceof pages.VerticalFlow){
-                    processVerticalFlow(widget);
-                }
-                else if(widget instanceof pages.ActionButton){
-                    processButton(widget);
-                }
+    return loadAsPromise(workingCopy.model().allProjectSecurities()[0]).then(
+        projectSecurity => {
+            var navigationDoc = workingCopy.model().allNavigationDocuments()[0];
+            console.log('Project Security Loaded');
+            projectSecurity.userRoles.forEach(role => {
+                jw.startElement(role.name);
                 
-            });
-        })
-    });
-    
+                if(navigationDoc != null){
+                    loadAsPromise(navigationDoc).then(
+                        navigation => {
+                            console.log(`Processing user navigation for: ${role.name}`);
+                            processNavigation(role, navigation);
+                        }
+                    ).then(jw.endElement());
+           
+            }
+        });
+        });
 }
 
-function processVerticalFlow(verticalFlow: pages.VerticalFlow){
+function traversePage(page: pages.IPage): when.Promise<void> {
+    console.log(`Traversing page: ${page.name}`);
+    return loadAsPromise(page).then(pageLoaded => {
+        jw.writeElement(page.name);
+        pageLoaded.traversePublicParts(visit => {
+            if (visit instanceof pages.ActionButton) {
+                processButton(visit);
+            }
+        })
+    }).then(_=>{jw.endElement()});
+
+}
+
+function processVerticalFlow(verticalFlow: pages.VerticalFlow) {
     verticalFlow.widgets.forEach(widget => {
-        if(widget instanceof pages.ActionButton){
-           processButton(widget);
+        if (widget instanceof pages.ActionButton) {
+            processButton(widget);
         }
     })
 }
-function processButton(button: pages.ActionButton){
-            var action = button.action;
-            if(action instanceof pages.MicroflowClientAction){
-                traverseMicroflow(action.microflowSettings.microflow);
-            }
-            else if (action instanceof pages.PageClientAction){
-                traversePage(action.pageSettings.page);
-            }       
+function processButton(button: pages.ActionButton): when.Promise<void> {
+    var action = button.action;
+    if (action instanceof pages.MicroflowClientAction) {
+        if(action.microflowSettings.microflow != null){
+            return traverseMicroflow(action.microflowSettings.microflow);
+        }
+    }
+    else if (action instanceof pages.PageClientAction) {
+        if(action.pageSettings.page != null){
+            return traversePage(action.pageSettings.page);
+        }
+    }
 }
 
 
-function traverseMicroflow(microflow: microflows.IMicroflow){
-    loadAsPromise(microflow).then(mf=>{
+function traverseMicroflow(microflow: microflows.IMicroflow): when.Promise<void> {
+    console.log(`Traversing microflow: ${microflow.name}`);
+    return loadAsPromise(microflow).then(mf=> {
         //process pages
         mf.objectCollection.objects.filter(o => o instanceof microflows.ShowPageAction).forEach(showPage => {
-           var activity = <microflows.ActionActivity> showPage;
-           var action = activity.action;
-            if(action instanceof microflows.ShowPageAction){
-               jw.writeElement(action.pageSettings.page.name);
-               traversePage(action.pageSettings.page);
-               jw.endElement();
-            }
-           });
-           //process show hompage action
-           mf.objectCollection.objects.filter(o => o instanceof microflows.ShowHomePageAction).forEach(showPage => {
-                jw.writeElement(`Show Homepage`);
+            var activity = <microflows.ActionActivity>showPage;
+            var action = activity.action;
+            if (action instanceof microflows.ShowPageAction) {
+                jw.writeElement(action.pageSettings.page.name);
+                traversePage(action.pageSettings.page);
                 jw.endElement();
-           });
-           //process show microflows
-           mf.objectCollection.objects.filter(o => o instanceof microflows.MicroflowCallAction).forEach((mfPara) => { 
-              
-           var activity = <microflows.ActionActivity> mfPara;
-           var action = activity.action;
-           
-            if(action instanceof microflows.MicroflowCallAction){
-               jw.writeElement(action.microflowCall.microflow.name);
-               traverseMicroflow(action.microflowCall.microflow);
-               jw.endElement();
             }
-           
-          });
-           
         });
-        
-    }
+        //process show hompage action
+        mf.objectCollection.objects.filter(o => o instanceof microflows.ShowHomePageAction).forEach(showPage => {
+            jw.writeElement(`Show Homepage`);
+            jw.endElement();
+        });
+        //process show microflows
+        mf.objectCollection.objects.filter(o => o instanceof microflows.MicroflowCallAction).forEach((mfPara) => {
+
+            var activity = <microflows.ActionActivity>mfPara;
+            var action = activity.action;
+
+            if (action instanceof microflows.MicroflowCallAction) {
+                jw.writeElement(action.microflowCall.microflow.name);
+                traverseMicroflow(action.microflowCall.microflow).then(_ =>{jw.endElement();});   
+            }
+
+        });
+
+    });
+
+}
