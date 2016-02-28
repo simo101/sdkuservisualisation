0cf508c8e4f128f852015112c71b56d2efaf3285
Simon Black <simonblack1@hotmail.com>
2016-02-28 12:25:50 +0000

    Working on traversal of pages.

--- script.ts
+++ script.ts
@@ -1,7 +1,7 @@
 /// <reference path='./typings/tsd.d.ts' />
 
 import { MendixSdkClient, OnlineWorkingCopy, Project, Revision, Branch, loadAsPromise } from "mendixplatformsdk";
-import { ModelSdkClient, IModel, projects, domainmodels, microflows, pages, navigation, texts, security } from "mendixmodelsdk";
+import { ModelSdkClient, IModel, projects, domainmodels, microflows, pages, navigation, texts, security, IStructure } from "mendixmodelsdk";
 import when = require('when');
 //const readlineSync = require('readline-sync');
 
@@ -25,16 +25,19 @@ const projectId = "bce92469-dd44-4414-a7fb-af659a2cee44";
 const projectName = "NaturesPrideDemo";
 const revNo = -1; // -1 for latest
 const branchName = null // null for mainline
-
+const wc = null;
 const client = new MendixSdkClient(username, apikey);
-
+var nav = <navigation.NavigationDocument>nav;
 /*
  * PROJECT TO ANALYZE
  */
 const project = new Project(client, projectId, projectName);
 
 client.platform().createOnlineWorkingCopy(project, new Revision(revNo, new Branch(project, branchName)))
-    .then(workingCopy => processUserNavigation(workingCopy))
+    .then(workingCopy => loadProjectSecurity(workingCopy))
+    .then(projectSecurity => getAllUserRoles(projectSecurity))
+    .then(userRoles => loadAllUserNavigation(userRoles))
+
     .done(
     () => {
         var jsonString = JSON.stringify(jsonObj);
@@ -51,76 +54,97 @@ function pickNavigationDocument(workingCopy: OnlineWorkingCopy): navigation.INav
     return workingCopy.model().allNavigationDocuments()[0];
 }
 
-function processNavigation(role: security.IUserRole, navDoc: navigation.NavigationDocument,element): when.Promise<void> {
-
-    let homepage = navDoc.desktopProfile.homePage;
-    let usersHomepage = navDoc.desktopProfile.roleBasedHomePages.filter(roleBasedHomepage => roleBasedHomepage.userRole.name === role.name)[0];
-    if (usersHomepage != null) {
-        if (usersHomepage.page != null) {
-            element[usersHomepage.page.name] = {};
-            console.log(`${role.name} homepage = ${usersHomepage.page.name}`);
-            return traversePage(usersHomepage.page,element[usersHomepage.page.name]);
-        } else if (usersHomepage.microflow != null) {
-            element[usersHomepage.microflow.name] = {};
-            console.log(`${role.name} homepage = ${usersHomepage.microflow.name}`);
-            return traverseMicroflow(usersHomepage.microflow,element[usersHomepage.microflow.name]);
-
-        }
-    }
-    return;
-};
-function processUserNavigation(workingCopy: OnlineWorkingCopy): when.Promise<void> {
-    // jw.startDocument('1.0', 'UTF-8');
-    return loadAsPromise(workingCopy.model().allProjectSecurities()[0]).then(
-        projectSecurity => {
-            var navigationDoc = workingCopy.model().allNavigationDocuments()[0];
-            if (navigationDoc != null) {
-                console.log('Project Security Loaded');
-                loadAsPromise(navigationDoc).then(navigation => {
-                    projectSecurity.userRoles.forEach(role => {
-                        jsonObj[role.name] = {};
-                        console.log(`Processing user navigation for: ${role.name}`);
-                        processNavigation(role, navigation,jsonObj[role.name]);
-                    }
-                    );
-
-
-                });
-
+function processNavigation(role: security.IUserRole, nav: navigation.INavigationDocument, element): when.Promise<void> {
+    return loadAsPromise(nav).then(navDoc => {
+        let usersHomepage = navDoc.desktopProfile.roleBasedHomePages.filter(roleBasedHomepage => roleBasedHomepage.userRole.name === role.name)[0];
+        if (usersHomepage != null) {
+            if (usersHomepage.page != null) {
+                console.log(`${role.name} homepage = ${usersHomepage.page.name}`);
+                return traversePage(usersHomepage.page, element);
+            } else if (usersHomepage.microflow != null) {
+                console.log(`${role.name} homepage = ${usersHomepage.microflow.name}`);
+                traverseMicroflow(usersHomepage.microflow, element);
             }
-        });
-
+        }
+        return;
+    });
+};
+function loadProjectSecurity(workingCopy: OnlineWorkingCopy): when.Promise<security.ProjectSecurity> {
+    return loadAsPromise(workingCopy.model().allProjectSecurities()[0]);
 }
 
-function traversePage(page: pages.IPage,element): when.Promise<void> {
+function loadAllUserNavigation(userRoles: security.UserRole[]): when.Promise<security.UserRole[]> {
+    return when.all<security.UserRole[]>(userRoles.map(processUsersNavigation));
+}
+function getAllUserRoles(projectSecurity: security.ProjectSecurity): security.UserRole[] {
+    return projectSecurity.userRoles;
+}
+
+function processUsersNavigation(role: security.UserRole): when.Promise<void> {
+    var nav = role.model.allNavigationDocuments()[0];
+    jsonObj[role.name] = {};
+    console.log(`Processing user navigation for: ${role.name}`);
+    return processNavigation(role, nav, jsonObj[role.name]);
+}
+
+
+function traversePage(page: pages.IPage, element): when.Promise<void> {
     console.log(`Traversing page: ${page.name}`);
     return loadAsPromise(page).then(pageLoaded => {
         element[page.name] = {};
-        pageLoaded.traversePublicParts(visit => {
-            if (visit instanceof pages.ActionButton) {
-                processButton(visit, element);
-            }
-        })
+        pageLoaded.traverse(function(structure){
+            traverseElement(element[page.name], structure);
+        });
     });
 
 }
-
-function processButton(button: pages.ActionButton,element): when.Promise<void> {
-    var action = button.action;
-    if (action instanceof pages.MicroflowClientAction) {
-        if (action.microflowSettings.microflow != null) {
-            return traverseMicroflow(action.microflowSettings.microflow,element);
+function traverseElement(element, structure: IStructure): when.Promise<void> {
+    if (structure != null) {
+        if (structure instanceof pages.Button) {
+           return processButton(structure, element);
+        } else if(structure instanceof pages.ControlBarButton){
+           return processControlBarButton(structure,element);
         }
-    }
-    else if (action instanceof pages.PageClientAction) {
-        if (action.pageSettings.page != null) {
+    } 
+        return;
+}
+
+function processControlBarButton(button: pages.ControlBarButton, element): when.Promise<void> {
+    if(button instanceof pages.GridEditButton){
+        return traversePage(button.pageSettings.page,element);
+    }else if(button instanceof pages.DataViewActionButton){
+        var action = button.action;
+        if(action instanceof pages.MicroflowClientAction){
+            return traverseMicroflow(action.microflowSettings.microflow,element);
+        }else if (action instanceof pages.PageClientAction){
             return traversePage(action.pageSettings.page,element);
         }
     }
 }
 
 
-function traverseMicroflow(microflow: microflows.IMicroflow,element): when.Promise<void> {
+function processButton(button: pages.Button, element): when.Promise<void> {
+    if(button instanceof pages.ActionButton){
+        var action = button.action;
+        if (action instanceof pages.MicroflowClientAction) {
+            if (action.microflowSettings.microflow != null) {
+                return traverseMicroflow(action.microflowSettings.microflow, element);
+            }
+        }
+        else if (action instanceof pages.PageClientAction) {
+            if (action.pageSettings.page != null) {
+                return traversePage(action.pageSettings.page, element);
+            }
+        }
+    }else if(button instanceof pages.DropDownButton){
+        
+    }else if(button instanceof pages.NewButton){
+        return traversePage(button.pageSettings.page,element);  
+    }
+}
+
+
+function traverseMicroflow(microflow: microflows.IMicroflow, element): when.Promise<void> {
     console.log(`Traversing microflow: ${microflow.name}`);
     return loadAsPromise(microflow).then(mf=> {
         //process pages
@@ -129,7 +153,7 @@ function traverseMicroflow(microflow: microflows.IMicroflow,element): when.Promi
             var action = activity.action;
             if (action instanceof microflows.ShowPageAction) {
                 element[action.pageSettings.page.name] = {};
-                traversePage(action.pageSettings.page,element[action.pageSettings.page.name]);
+                traversePage(action.pageSettings.page, element[action.pageSettings.page.name]);
             }
         });
         //process show hompage action
@@ -144,7 +168,7 @@ function traverseMicroflow(microflow: microflows.IMicroflow,element): when.Promi
 
             if (action instanceof microflows.MicroflowCallAction) {
                 element[action.microflowCall.microflow.name] = {};
-                traverseMicroflow(action.microflowCall.microflow,element[action.microflowCall.microflow.name]);
+                traverseMicroflow(action.microflowCall.microflow, element[action.microflowCall.microflow.name]);
             }
 
         });
