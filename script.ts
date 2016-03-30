df14d63d2aa81cd9930964b41cda114a2af30438
Simon Black <simonblack1@hotmail.com>
2016-03-30 20:38:22 +0100

    Working on improving code to handle nulls and more complex apps.

--- script.ts
+++ script.ts
@@ -9,7 +9,7 @@ var ws = fs.createWriteStream('./web/mendix.json');
 const jsonObj = {};
 
 const username = "{{UserName}}";
-const apikey = "{{APIKEY}}";
+const apikey = "{{ApiKey}}";
 const projectId = "{{ProjectID}}";
 const projectName = "{{ProjectName}}";
 const revNo = -1; // -1 for latest
@@ -74,24 +74,64 @@ function processNavigation(role: security.UserRole, navDoc: navigation.Navigatio
                 return processOtherNavigation(navDoc, element, role);
             });
         }
-    }
-};
+        else {
+            return;
+        }
+    } else {
+        let defaultHomepage = navDoc.desktopProfile.homePage;
+        if (defaultHomepage != null) {
+            if (defaultHomepage.page != null) {
+                console.log(`${role.name} homepage = ${defaultHomepage.page.name}`);
+                return loadAsPromise(defaultHomepage.page).then(pg => processButtons(element, pg, role)).then(_ => {
+                    return processOtherNavigation(navDoc, element, role);
+                });
+            } else if (defaultHomepage.microflow != null) {
+                console.log(`${role.name} homepage = ${defaultHomepage.microflow.name}`);
+                return loadAsPromise(defaultHomepage.microflow).then(mf => traverseMicroflow(mf, element, role)).then(_ => {
+                    return processOtherNavigation(navDoc, element, role);
+                });
+            }
+            else {
+                return;
+            }
+        } else {
+            return;
+        }
+    };
+}
 /**
 * This function processes the other users navigation
 */
 function processOtherNavigation(navDoc: navigation.NavigationDocument, element, userRole): when.Promise<void> {
     var items = navDoc.desktopProfile.menuItemCollection.items;
-    return when.all<void>(items.map(item => processItem(item, element, userRole)));
+    if (items != null) {
+        return when.all<void>(items.map(item => processItem(item, element, userRole)));
+    }
+    else {
+        return;
+    }
 }
 /**
 * This function processes a menu item.
 */
 function processItem(item: menus.MenuItem, element, role: security.UserRole): when.Promise<void> {
     var action = item.action;
-    if (action instanceof pages.PageClientAction) {
-        return loadAsPromise(action.pageSettings.page).then(pg => processButtons(element, pg, role))
-    } else if (action instanceof pages.MicroflowClientAction) {
-        return loadAsPromise(action.microflowSettings.microflow).then(mf => traverseMicroflow(mf, element, role));
+    if (action != null) {
+        if (action instanceof pages.PageClientAction) {
+            if (action.pageSettings.page != null) {
+                return loadAsPromise(action.pageSettings.page).then(pg => processButtons(element, pg, role));
+            } else {
+                return;
+            }
+        } else if (action instanceof pages.MicroflowClientAction) {
+            if (action.microflowSettings.microflow != null) {
+                return loadAsPromise(action.microflowSettings.microflow).then(mf => traverseMicroflow(mf, element, role));
+            } else {
+                return;
+            }
+        }
+    } else {
+        return;
     }
 }
 
@@ -136,7 +176,11 @@ function processUsersNavigation(role: security.UserRole): when.Promise<void> {
     var child = { name: role.name, children: [], parent: "User Roles" };
     jsonObj[`children`].push(child);
     console.log(`Processing user navigation for: ${role.name}`);
-    return loadAsPromise(nav).then(loadedNav => processNavigation(role, loadedNav, child));
+    if (nav != null) {
+        return loadAsPromise(nav).then(loadedNav => processNavigation(role, loadedNav, child));
+    } else {
+        return;
+    }
 }
 
 /**
@@ -157,19 +201,26 @@ function getStructures(pageLoaded: pages.Page, element): IStructure[] {
 * This function processes a button and adds it to the jsonObj.
 */
 function processButtons(element, page: pages.Page, userRole: security.UserRole): when.Promise<void> {
-    if (checkPageSecurity(page, userRole)) {
-        var buttons = getStructures(page, element);
-        var child = { name: page.name, children: [], parent: element.name };
-        element["children"].push(child);
-        if (!checkIfInElement(page.name, element)) {
-            return when.all<void>(buttons.map(btn => traverseElement(child, btn, userRole)));
+    if (page != null) {
+        if (checkPageSecurity(page, userRole)) {
+            var buttons = getStructures(page, element);
+
+            if (buttons.length > 0) {
+                if (!checkIfInElement(page.name, element)) {
+                    var child = { name: page.name, children: [], parent: element.name };
+                    element["children"].push(child);
+                    return when.all<void>(buttons.map(btn => traverseElement(child, btn, userRole)));
+                } else {
+                    return;
+                }
+            }
+            else {
+                return;
+            }
         } else {
             return;
         }
-    } else {
-        return;
     }
-
 }
 /**
 * This function traverses a page element
@@ -181,6 +232,8 @@ function traverseElement(element, structure: IStructure, userRole: security.User
         } else if (structure instanceof pages.ControlBarButton) {
             return processControlBarButton(structure, element, userRole);
         }
+    } else {
+        return;
     }
 }
 /**
@@ -188,13 +241,31 @@ function traverseElement(element, structure: IStructure, userRole: security.User
 */
 function processControlBarButton(button: pages.ControlBarButton, element, userRole: security.UserRole): when.Promise<void> {
     if (button instanceof pages.GridEditButton) {
-        return loadAsPromise(button.pageSettings.page).then(pg => processButtons(element, pg, userRole));
+        if (button.pageSettings.page != null) {
+            return loadAsPromise(button.pageSettings.page).then(pg => processButtons(element, pg, userRole));
+        } else {
+            return;
+        }
     } else if (button instanceof pages.DataViewActionButton) {
         var action = button.action;
-        if (action instanceof pages.MicroflowClientAction) {
-            return loadAsPromise(action.microflowSettings.microflow).then(mf => traverseMicroflow(mf, element, userRole));
-        } else if (action instanceof pages.PageClientAction) {
-            return loadAsPromise(action.pageSettings.page).then(pg => processButtons(element, pg, userRole));
+        if (action != null) {
+            if (action instanceof pages.MicroflowClientAction) {
+                if (action.microflowSettings.microflow != null) {
+                    return loadAsPromise(action.microflowSettings.microflow).then(mf => traverseMicroflow(mf, element, userRole));
+                }
+                else {
+                    return;
+                }
+            } else if (action instanceof pages.PageClientAction) {
+                if (action.pageSettings.page != null) {
+                    return loadAsPromise(action.pageSettings.page).then(pg => processButtons(element, pg, userRole));
+                } else {
+                    return;
+                }
+            }
+        }
+        else {
+            return;
         }
     }
 }
@@ -205,28 +276,37 @@ function processControlBarButton(button: pages.ControlBarButton, element, userRo
 function processButton(button: pages.Button, element, userRole: security.UserRole): when.Promise<void> {
     if (button instanceof pages.ActionButton) {
         var action = button.action;
-        if (action instanceof pages.MicroflowClientAction) {
-            if (action.microflowSettings.microflow != null) {
-                return loadAsPromise(action.microflowSettings.microflow).then(mf => traverseMicroflow(mf, element, userRole));
+        if (action != null) {
+            if (action instanceof pages.MicroflowClientAction) {
+                if (action.microflowSettings.microflow != null) {
+                    return loadAsPromise(action.microflowSettings.microflow).then(mf => traverseMicroflow(mf, element, userRole));
+                }
             }
-        }
-        else if (action instanceof pages.PageClientAction) {
-            if (action.pageSettings.page != null) {
-                return loadAsPromise(action.pageSettings.page).then(pg => processButtons(element, pg, userRole));
+            else if (action instanceof pages.PageClientAction) {
+                if (action.pageSettings.page != null) {
+                    return loadAsPromise(action.pageSettings.page).then(pg => processButtons(element, pg, userRole));
+                }
             }
+        } else {
+            return;
         }
     } else if (button instanceof pages.DropDownButton) {
 
     } else if (button instanceof pages.NewButton) {
         return loadAsPromise(button.pageSettings.page).then(pg => {
             var entity = button.entity;
-            loadAsPromise(entity).then(ent => {
-                if (checkEntitySecurityCanCreate(ent, userRole)) {
-                    return processButtons(element, pg, userRole);
-                } else {
-                    return;
-                }
-            });
+            if (entity != null) {
+                loadAsPromise(entity).then(ent => {
+                    if (checkEntitySecurityCanCreate(ent, userRole)) {
+                        return processButtons(element, pg, userRole);
+                    } else {
+                        return;
+                    }
+                });
+            } else {
+                return;
+            }
+
         });
 
     }
@@ -244,18 +324,31 @@ function processAction(mfObj: microflows.IMicroflowObject, element, userRole: se
     if (mfObj instanceof microflows.ActionActivity) {
 
         var action = mfObj.action;
-        if (action instanceof microflows.ShowPageAction) {
-            console.log(`Microflow action to open page ${action.pageSettings.page.name}`);
-            return loadAsPromise(action.pageSettings.page).then(pg => processButtons(element, pg, userRole));
-        }
-        else if (action instanceof microflows.ShowHomePageAction) {
-            var child = { name: "ShowHomepage", children: [], parent: element.name };
-            element["children"].push(child);
+        if (action != null) {
+            if (action instanceof microflows.ShowPageAction) {
+                console.log(`Microflow action to open page ${action.pageSettings.page.name}`);
+                if (action.pageSettings.page != null) {
+                    return loadAsPromise(action.pageSettings.page).then(pg => processButtons(element, pg, userRole));
+                } else {
+                    return;
+                }
+            }
+            else if (action instanceof microflows.ShowHomePageAction) {
+                var child = { name: "ShowHomepage", children: [], parent: element.name };
+                element["children"].push(child);
+                return;
+            } else if (action instanceof microflows.MicroflowCallAction) {
+                console.log(`Microflow action to open microflow ${action.microflowCall.microflow.name}`);
+                if (action.microflowCall.microflow != null) {
+                    return loadAsPromise(action.microflowCall.microflow).then(mf => traverseMicroflow(mf, element, userRole));
+                } else {
+                    return;
+                }
+            }
+        } else {
             return;
-        } else if (action instanceof microflows.MicroflowCallAction) {
-            console.log(`Microflow action to open microflow ${action.microflowCall.microflow.name}`);
-            return loadAsPromise(action.microflowCall.microflow).then(mf => traverseMicroflow(mf, element, userRole));
         }
+
     }
 }
 /**
@@ -264,9 +357,9 @@ function processAction(mfObj: microflows.IMicroflowObject, element, userRole: se
 function traverseMicroflow(microflow: microflows.Microflow, element, userRole: security.UserRole): when.Promise<void> {
     if (checkMicroflowSecurity(microflow, userRole)) {
         console.log(`Traversing Microflow for: ${microflow.name}`);
-        var child = { name: microflow.name, children: [], parent: element.name };
-        element["children"].push(child);
         if (!checkIfInElement(microflow.name, element)) {
+            var child = { name: microflow.name, children: [], parent: element.name };
+            element["children"].push(child);
             return traverseMicroflowActions(microflow.objectCollection.objects.filter(o => o instanceof microflows.ActionActivity), child, userRole);
         } else {
             return;
@@ -283,12 +376,16 @@ function traverseMicroflow(microflow: microflows.Microflow, element, userRole: s
 function checkPageSecurity(page: pages.Page, userRole: security.UserRole): boolean {
     var moduleRolesAllowed = page.allowedRolesQualifiedNames;
     var userRolesModuleRoles = userRole.moduleRolesQualifiedNames;
-    moduleRolesAllowed.forEach(role => {
-        if (userRolesModuleRoles.filter(usersModuleRole => usersModuleRole === role).length >= 1) {
-            return true;
+    var i;
+    var a;
+    for (i = 0; i < moduleRolesAllowed.length; i++) {
+        for (a = 0; a < userRolesModuleRoles.length; a++) {
+            if (userRolesModuleRoles[a] === moduleRolesAllowed[i]) {
+                return true;
+            }
         }
-    });
-    return true;
+    }
+    return false;
 }
 
 /**
@@ -297,27 +394,38 @@ function checkPageSecurity(page: pages.Page, userRole: security.UserRole): boole
 function checkMicroflowSecurity(microflow: microflows.Microflow, userRole: security.UserRole): boolean {
     var moduleRolesAllowed = microflow.allowedModuleRolesQualifiedNames;
     var userRolesModuleRoles = userRole.moduleRolesQualifiedNames;
-    moduleRolesAllowed.forEach(role => {
-        if (userRolesModuleRoles.filter(usersModuleRole => usersModuleRole === role).length >= 1) {
-            return true;
+    var i;
+    var a;
+    for (i = 0; i < moduleRolesAllowed.length; i++) {
+        for (a = 0; a < userRolesModuleRoles.length; a++) {
+            if (userRolesModuleRoles[a] === moduleRolesAllowed[i]) {
+                return true;
+            }
         }
-    });
-    return true;
+    }
+    return false;
 }
+
+
 /**
 * This function checks to see whether a given user role is able to create the given entity.
 */
 function checkEntitySecurityCanCreate(entity: domainmodels.Entity, userRole: security.UserRole): boolean {
     var accessRules = entity.accessRules;
     var userRolesModuleRoles = userRole.moduleRolesQualifiedNames;
-    userRolesModuleRoles.forEach(role => {
-        var filteredAccessRules = accessRules.filter(accessRule => accessRule.moduleRoles.filter(moduleRole => moduleRole.qualifiedName === role).length >= 1)
-        filteredAccessRules.forEach(filteredAccessRule => {
-            if (filteredAccessRule.allowCreate) {
-                return true;
+    var i, a, b;
+    for (i = 0; i < userRolesModuleRoles.length; i++) {
+        for (a = 0; a < accessRules.length; a++) {
+            for (b = 0; b < accessRules[a].moduleRoles.length; b++) {
+                if (userRolesModuleRoles[i] === accessRules[a].moduleRoles[b].qualifiedName) {
+                    if (accessRules[i].allowCreate) {
+                        return true;
+                    }
+
+                }
             }
-        });
-    });
+        }
+    }
     return false;
 }
 
@@ -341,10 +449,21 @@ function checkEntitySecurityCanDelete(entity: domainmodels.Entity, userRole: sec
 /**
 * This function checks to see if the new element already exists in the jsonObj.
 * Returns true if the element exists as a parent of the current element.
+* This function also checks to see if it exists a child already.
 */
 function checkIfInElement(newElement: String, element): boolean {
-    if (element.parent === newElement) {
+    if (element.parent === newElement || checkIfInChildren(newElement, element)) {
         return true;
+    }
+    return false;
+}
+
+function checkIfInChildren(newElement: String, element): boolean {
+    var i;
+    for (i = 0; i < element.children.length; i++) {
+        if (element.children[i].name === newElement) {
+            return true;
+        }
     }
     return false;
 }
